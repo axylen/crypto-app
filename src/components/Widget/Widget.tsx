15989a138ee4c5b9fece3d528f52461b2bea35f5
Dmitry Bullow <dmitry55580@gmail.com>
2022-03-20 12:27:23 +0300

    Create Widget component

---
+++ src/components/Widget/Widget.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { cx } from "utils";
+import css from "./Widget.module.css";
+
+type Props = {
+  className?: string;
+};
+
+export const Widget: React.FC<Props> = (props) => {
+  const { className } = props;
+
+  return <div className={cx(className, css.Widget)}>{props.children}</div>;
+};
